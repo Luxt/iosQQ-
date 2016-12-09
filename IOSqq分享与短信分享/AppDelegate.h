6c1452d8eb6359655f535dc8a0bbde3a89fe0a49
lxt <292327576@qq.com>
2016-12-09 09:09:41 +0800

    Initial Commit

---
+++ IOSqq分享与短信分享/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSqq分享与短信分享
+//
+//  Created by zmit on 16/12/9.
+//  Copyright © 2016年 zmit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSqq分享与短信分享/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOSqq分享与短信分享
+//
+//  Created by zmit on 16/12/9.
+//  Copyright © 2016年 zmit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
